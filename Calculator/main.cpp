bd5e0febecbcef45b54d0e4d6c13470f5e392124
Justyn Durnford <jpdurnford@alaska.edu>
2019-10-28 10:06:10 -0700

    Working on GUI in main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -2,37 +2,42 @@
 //Justyn P. Durnford
 //Izac Lorimer
 //Created on 9/30/2019
-//Last Updated on 10/25/2019
+//Last Updated on 10/28/2019
 //Main file for calculator project.
 
 #include "mathFunctions.hpp"
+#include <vector>
+#include <string>
 #include <iostream>
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Output.H>
 
-/*
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+
 int main(int argc, char** argv)
 {
-	Fl_Window* window = new Fl_Window(500, 600); //Length, Height
-	Fl_Box* box = new Fl_Box(20, 20, 460, 100, "Hello, World!"); //X, Y, Length, Height
-	Fl_Button* button = new Fl_Button(30, 30, 50, 50); //X, Y, Length, Height
-	Fl_Output* output = new Fl_Output(30, 150, 100, 50); //X, Y, Length, Height
-	box->box(FL_BORDER_BOX);
-	box->labelfont(FL_BOLD);
-	box->labelsize(36);
-	box->labeltype(FL_SHADOW_LABEL);
-	window->end();
-	window->show(argc, argv);
+	Fl_Window* mainWindow = new Fl_Window(500, 600); //Length, Height
+	Fl_Output* output = new Fl_Output(20, 20, 100, 50); //X, Y, Length, Height
 
+	
+	Fl_Button* button0 = new Fl_Button(40, 60, 40, 40, "0");
+
+	output->value("Hey");
+	mainWindow->end();
+	mainWindow->show(argc, argv);
 	return Fl::run();
 }
-*/
 
+/*
 int main()
 {
 	return 0;
 }
+*/

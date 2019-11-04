29e2abd3e4e9376db89f2e906890ed1228e62186
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-04 09:35:24 -0800

    Added number buttons in main() in main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -21,17 +21,26 @@ using std::cout;
 using std::endl;
 
 
-int main(int argc, char** argv)
+int main()
 {
 	Fl_Window* mainWindow = new Fl_Window(500, 600); //Length, Height
-	Fl_Output* output = new Fl_Output(20, 20, 100, 50); //X, Y, Length, Height
+	Fl_Output* output = new Fl_Output(20, 20, 460, 40); //X, Y, Length, Height
 
-	
-	Fl_Button* button0 = new Fl_Button(40, 60, 40, 40, "0");
+	Fl_Button* button1 = new Fl_Button(340, 80, 40, 40, "1");
+	Fl_Button* button2 = new Fl_Button(380, 80, 40, 40, "2");
+	Fl_Button* button3 = new Fl_Button(420, 80, 40, 40, "3");
+	Fl_Button* button4 = new Fl_Button(340, 120, 40, 40, "4");
+	Fl_Button* button5 = new Fl_Button(380, 120, 40, 40, "5");
+	Fl_Button* button6 = new Fl_Button(420, 120, 40, 40, "6");
+	Fl_Button* button7 = new Fl_Button(340, 160, 40, 40, "7");
+	Fl_Button* button8 = new Fl_Button(380, 160, 40, 40, "8");
+	Fl_Button* button9 = new Fl_Button(420, 160, 40, 40, "9");
+
+	Fl_Button* button0 = new Fl_Button(40, 80, 40, 40, "0");
 
 	output->value("Hey");
 	mainWindow->end();
-	mainWindow->show(argc, argv);
+	mainWindow->show();
 	return Fl::run();
 }
 
